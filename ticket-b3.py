1fa04f0a434550d2c8ed4a22b068ca618a26d857
Jefferson Barreto <33812040+jeffbarreto1@users.noreply.github.com>
2021-10-21 15:11:35 -0300

    Carteira de ações, consulta
    Consultando uma lista de ações

---
+++ ticket-b3.py
@@ -0,0 +1,15 @@
+from pandas_datareader import data
+import pandas as pd
+from IPython.display import display
+from datetime import date
+from tkinter import *
+
+
+
+ticket_b3 = ['BIDI4.SA','ITSA4.SA', 'ALPK3.SA', 'FLRY3.SA', 'OIBR3.SA']
+
+for ticket in ticket_b3:
+    print(ticket)
+    cotacao_bovespa = data.DataReader(ticket, data_source='yahoo', start=date.today(), end=date.today())
+    display(cotacao_bovespa)
+    print("\n")
